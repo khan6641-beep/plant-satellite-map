8672de7493d3cafe4c8ae5be279fadb9e1a51cfb
khan6641-beep <khan6641@gmail.com>
2026-07-21 15:14:33 +0900

    Add files via upload

--- data/plant-images.js
+++ data/plant-images.js
@@ -1,20 +1,442 @@
 /*
- * 식물 대표 이미지 연결 파일입니다.
- * 이미지 파일은 assets/images/plants/ 폴더에 넣고 아래 예시처럼 등록하세요.
- * 우선순위: 표찰번호 > 국명 > 학명 > 기본 이미지
- *
- * 예시:
- * byOrigin: {
- *   "12-L-00001": {
- *     src: "./assets/images/plants/12-L-00001.jpg",
- *     alt: "참느릅나무 대표 이미지",
- *     credit: "촬영: 홍길동",
- *     license: "국립생태원 내부 활용"
- *   }
- * }
+ * 援?챸蹂?????대?吏 ?곌껐 ?뚯씪?낅땲??
+ * ?대?吏 ?뚯씪? assets/images/plants/ ?대뜑???섎Т紐낆쑝濡???ν빀?덈떎.
  */
 window.PLANT_IMAGE_MAP = {
   byOrigin: {},
-  byCommonName: {},
+  byCommonName: {
+    "가막살나무": {
+      src: "./assets/images/plants/가막살나무.JPG",
+      alt: "가막살나무 ????대?吏"
+    },
+    "가문비나무": {
+      src: "./assets/images/plants/가문비나무.JPG",
+      alt: "가문비나무 ????대?吏"
+    },
+    "가침박달": {
+      src: "./assets/images/plants/가침박달.JPG",
+      alt: "가침박달 ????대?吏"
+    },
+    "갈참나무": {
+      src: "./assets/images/plants/갈참나무.jpg",
+      alt: "갈참나무 ????대?吏"
+    },
+    "감나무": {
+      src: "./assets/images/plants/감나무.JPG",
+      alt: "감나무 ????대?吏"
+    },
+    "개서어나무": {
+      src: "./assets/images/plants/개서어나무.JPG",
+      alt: "개서어나무 ????대?吏"
+    },
+    "개암나무": {
+      src: "./assets/images/plants/개암나무.jpg",
+      alt: "개암나무 ????대?吏"
+    },
+    "갯버들": {
+      src: "./assets/images/plants/갯버들.jpg",
+      alt: "갯버들 ????대?吏"
+    },
+    "고로쇠나무": {
+      src: "./assets/images/plants/고로쇠나무.JPG",
+      alt: "고로쇠나무 ????대?吏"
+    },
+    "고욤나무": {
+      src: "./assets/images/plants/고욤나무.JPG",
+      alt: "고욤나무 ????대?吏"
+    },
+    "곰솔": {
+      src: "./assets/images/plants/곰솔.JPG",
+      alt: "곰솔 ????대?吏"
+    },
+    "광나무": {
+      src: "./assets/images/plants/광나무.jpg",
+      alt: "광나무 ????대?吏"
+    },
+    "구상나무": {
+      src: "./assets/images/plants/구상나무.png",
+      alt: "구상나무 ????대?吏"
+    },
+    "굴참나무": {
+      src: "./assets/images/plants/굴참나무.JPG",
+      alt: "굴참나무 ????대?吏"
+    },
+    "굴피나무": {
+      src: "./assets/images/plants/굴피나무.JPG",
+      alt: "굴피나무 ????대?吏"
+    },
+    "까마귀밥나무": {
+      src: "./assets/images/plants/까마귀밥나무.JPG",
+      alt: "까마귀밥나무 ????대?吏"
+    },
+    "꾸지나무": {
+      src: "./assets/images/plants/꾸지나무.JPG",
+      alt: "꾸지나무 ????대?吏"
+    },
+    "꾸지뽕나무": {
+      src: "./assets/images/plants/꾸지뽕나무.jpg",
+      alt: "꾸지뽕나무 ????대?吏"
+    },
+    "난티잎개암나무": {
+      src: "./assets/images/plants/난티잎개암나무.JPG",
+      alt: "난티잎개암나무 ????대?吏"
+    },
+    "노각나무": {
+      src: "./assets/images/plants/노각나무.JPG",
+      alt: "노각나무 ????대?吏"
+    },
+    "노린재나무": {
+      src: "./assets/images/plants/노린재나무.jpg",
+      alt: "노린재나무 ????대?吏"
+    },
+    "느티나무": {
+      src: "./assets/images/plants/느티나무.JPG",
+      alt: "느티나무 ????대?吏"
+    },
+    "다릅나무": {
+      src: "./assets/images/plants/다릅나무.JPG",
+      alt: "다릅나무 ????대?吏"
+    },
+    "당단풍나무": {
+      src: "./assets/images/plants/당단풍나무.JPG",
+      alt: "당단풍나무 ????대?吏"
+    },
+    "대추나무": {
+      src: "./assets/images/plants/대추나무.JPG",
+      alt: "대추나무 ????대?吏"
+    },
+    "대팻집나무": {
+      src: "./assets/images/plants/대팻집나무.JPG",
+      alt: "대팻집나무 ????대?吏"
+    },
+    "덜꿩나무": {
+      src: "./assets/images/plants/덜꿩나무.JPG",
+      alt: "덜꿩나무 ????대?吏"
+    },
+    "동백나무": {
+      src: "./assets/images/plants/동백나무.jpg",
+      alt: "동백나무 ????대?吏"
+    },
+    "들메나무": {
+      src: "./assets/images/plants/들메나무.JPG",
+      alt: "들메나무 ????대?吏"
+    },
+    "때죽나무": {
+      src: "./assets/images/plants/때죽나무.JPG",
+      alt: "때죽나무 ????대?吏"
+    },
+    "떡갈나무": {
+      src: "./assets/images/plants/떡갈나무.JPG",
+      alt: "떡갈나무 ????대?吏"
+    },
+    "뜰보리수": {
+      src: "./assets/images/plants/뜰보리수.JPG",
+      alt: "뜰보리수 ????대?吏"
+    },
+    "마가목": {
+      src: "./assets/images/plants/마가목.JPG",
+      alt: "마가목 ????대?吏"
+    },
+    "말오줌때": {
+      src: "./assets/images/plants/말오줌때.JPG",
+      alt: "말오줌때 ????대?吏"
+    },
+    "매실나무": {
+      src: "./assets/images/plants/매실나무.JPG",
+      alt: "매실나무 ????대?吏"
+    },
+    "멀구슬나무": {
+      src: "./assets/images/plants/멀구슬나무.JPG",
+      alt: "멀구슬나무 ????대?吏"
+    },
+    "모감주나무": {
+      src: "./assets/images/plants/모감주나무.JPG",
+      alt: "모감주나무 ????대?吏"
+    },
+    "모과나무": {
+      src: "./assets/images/plants/모과나무.JPG",
+      alt: "모과나무 ????대?吏"
+    },
+    "물오리나무": {
+      src: "./assets/images/plants/물오리나무.JPG",
+      alt: "물오리나무 ????대?吏"
+    },
+    "물푸레나무": {
+      src: "./assets/images/plants/물푸레나무.JPG",
+      alt: "물푸레나무 ????대?吏"
+    },
+    "박태기나무": {
+      src: "./assets/images/plants/박태기나무.JPG",
+      alt: "박태기나무 ????대?吏"
+    },
+    "밤나무": {
+      src: "./assets/images/plants/밤나무.jpg",
+      alt: "밤나무 ????대?吏"
+    },
+    "백당나무": {
+      src: "./assets/images/plants/백당나무.JPG",
+      alt: "백당나무 ????대?吏"
+    },
+    "버드나무": {
+      src: "./assets/images/plants/버드나무.JPG",
+      alt: "버드나무 ????대?吏"
+    },
+    "버들개회나무": {
+      src: "./assets/images/plants/버들개회나무.jpg",
+      alt: "버들개회나무 ????대?吏"
+    },
+    "벚나무": {
+      src: "./assets/images/plants/벚나무.JPG",
+      alt: "벚나무 ????대?吏"
+    },
+    "병꽃나무": {
+      src: "./assets/images/plants/병꽃나무.JPG",
+      alt: "병꽃나무 ????대?吏"
+    },
+    "보리수나무": {
+      src: "./assets/images/plants/보리수나무.JPG",
+      alt: "보리수나무 ????대?吏"
+    },
+    "복사나무": {
+      src: "./assets/images/plants/복사나무.jpg",
+      alt: "복사나무 ????대?吏"
+    },
+    "복자기": {
+      src: "./assets/images/plants/복자기.jpg",
+      alt: "복자기 ????대?吏"
+    },
+    "분비나무": {
+      src: "./assets/images/plants/분비나무.jpg",
+      alt: "분비나무 ????대?吏"
+    },
+    "붉가시나무": {
+      src: "./assets/images/plants/붉가시나무.jpg",
+      alt: "붉가시나무 ????대?吏"
+    },
+    "붉나무": {
+      src: "./assets/images/plants/붉나무.jpg",
+      alt: "붉나무 ????대?吏"
+    },
+    "뽕나무": {
+      src: "./assets/images/plants/뽕나무.jpg",
+      alt: "뽕나무 ????대?吏"
+    },
+    "사람주나무": {
+      src: "./assets/images/plants/사람주나무.jpg",
+      alt: "사람주나무 ????대?吏"
+    },
+    "산개벚지나무": {
+      src: "./assets/images/plants/산개벚지나무.jpg",
+      alt: "산개벚지나무 ????대?吏"
+    },
+    "산돌배나무": {
+      src: "./assets/images/plants/산돌배나무.jpg",
+      alt: "산돌배나무 ????대?吏"
+    },
+    "산딸나무": {
+      src: "./assets/images/plants/산딸나무.jpg",
+      alt: "산딸나무 ????대?吏"
+    },
+    "산벚나무": {
+      src: "./assets/images/plants/산벚나무.jpg",
+      alt: "산벚나무 ????대?吏"
+    },
+    "산뽕나무": {
+      src: "./assets/images/plants/산뽕나무.jpg",
+      alt: "산뽕나무 ????대?吏"
+    },
+    "산수유": {
+      src: "./assets/images/plants/산수유.jpg",
+      alt: "산수유 ????대?吏"
+    },
+    "산초나무": {
+      src: "./assets/images/plants/산초나무.jpg",
+      alt: "산초나무 ????대?吏"
+    },
+    "살구나무": {
+      src: "./assets/images/plants/살구나무.jpg",
+      alt: "살구나무 ????대?吏"
+    },
+    "상수리나무": {
+      src: "./assets/images/plants/상수리나무.jpg",
+      alt: "상수리나무 ????대?吏"
+    },
+    "생강나무": {
+      src: "./assets/images/plants/생강나무.jpg",
+      alt: "생강나무 ????대?吏"
+    },
+    "서어나무": {
+      src: "./assets/images/plants/서어나무.jpg",
+      alt: "서어나무 ????대?吏"
+    },
+    "소나무": {
+      src: "./assets/images/plants/소나무.jpg",
+      alt: "소나무 ????대?吏"
+    },
+    "소사나무": {
+      src: "./assets/images/plants/소사나무.jpg",
+      alt: "소사나무 ????대?吏"
+    },
+    "쇠물푸레": {
+      src: "./assets/images/plants/쇠물푸레.jpg",
+      alt: "쇠물푸레 ????대?吏"
+    },
+    "수수꽃다리": {
+      src: "./assets/images/plants/수수꽃다리.jpg",
+      alt: "수수꽃다리 ????대?吏"
+    },
+    "신갈나무": {
+      src: "./assets/images/plants/신갈나무.jpg",
+      alt: "신갈나무 ????대?吏"
+    },
+    "신나무": {
+      src: "./assets/images/plants/신나무.jpg",
+      alt: "신나무 ????대?吏"
+    },
+    "아왜나무": {
+      src: "./assets/images/plants/아왜나무.jpg",
+      alt: "아왜나무 ????대?吏"
+    },
+    "오갈피나무": {
+      src: "./assets/images/plants/오갈피나무.jpg",
+      alt: "오갈피나무 ????대?吏"
+    },
+    "오리나무": {
+      src: "./assets/images/plants/오리나무.jpg",
+      alt: "오리나무 ????대?吏"
+    },
+    "왕": {
+      src: "./assets/images/plants/왕.jpg",
+      alt: "왕 ????대?吏"
+    },
+    "왕버들": {
+      src: "./assets/images/plants/왕버들.jpg",
+      alt: "왕버들 ????대?吏"
+    },
+    "왕벚나무": {
+      src: "./assets/images/plants/왕벚나무.jpg",
+      alt: "왕벚나무 ????대?吏"
+    },
+    "은사시나무": {
+      src: "./assets/images/plants/은사시나무.jpg",
+      alt: "은사시나무 ????대?吏"
+    },
+    "은행나무": {
+      src: "./assets/images/plants/은행나무.jpg",
+      alt: "은행나무 ????대?吏"
+    },
+    "음나무": {
+      src: "./assets/images/plants/음나무.jpg",
+      alt: "음나무 ????대?吏"
+    },
+    "이팝나무": {
+      src: "./assets/images/plants/이팝나무.jpg",
+      alt: "이팝나무 ????대?吏"
+    },
+    "자귀나무": {
+      src: "./assets/images/plants/자귀나무.jpg",
+      alt: "자귀나무 ????대?吏"
+    },
+    "자도나무": {
+      src: "./assets/images/plants/자도나무.jpg",
+      alt: "자도나무 ????대?吏"
+    },
+    "자작나무": {
+      src: "./assets/images/plants/자작나무.jpg",
+      alt: "자작나무 ????대?吏"
+    },
+    "잔털벚나무": {
+      src: "./assets/images/plants/잔털벚나무.jpg",
+      alt: "잔털벚나무 ????대?吏"
+    },
+    "잣나무": {
+      src: "./assets/images/plants/잣나무.jpg",
+      alt: "잣나무 ????대?吏"
+    },
+    "전나무": {
+      src: "./assets/images/plants/전나무.jpg",
+      alt: "전나무 ????대?吏"
+    },
+    "조록싸리": {
+      src: "./assets/images/plants/조록싸리.jpg",
+      alt: "조록싸리 ????대?吏"
+    },
+    "졸참나무": {
+      src: "./assets/images/plants/졸참나무.jpg",
+      alt: "졸참나무 ????대?吏"
+    },
+    "종가시나무": {
+      src: "./assets/images/plants/종가시나무.jpg",
+      alt: "종가시나무 ????대?吏"
+    },
+    "주목": {
+      src: "./assets/images/plants/주목.jpg",
+      alt: "주목 ????대?吏"
+    },
+    "쥐똥나무": {
+      src: "./assets/images/plants/쥐똥나무.jpg",
+      alt: "쥐똥나무 ????대?吏"
+    },
+    "진달래": {
+      src: "./assets/images/plants/진달래.jpg",
+      alt: "진달래 ????대?吏"
+    },
+    "쪽동백나무": {
+      src: "./assets/images/plants/쪽동백나무.jpg",
+      alt: "쪽동백나무 ????대?吏"
+    },
+    "참느릅나무": {
+      src: "./assets/images/plants/참느릅나무.jpg",
+      alt: "참느릅나무 ????대?吏"
+    },
+    "참빗살나무": {
+      src: "./assets/images/plants/참빗살나무.jpg",
+      alt: "참빗살나무 ????대?吏"
+    },
+    "철쭉": {
+      src: "./assets/images/plants/철쭉.jpg",
+      alt: "철쭉 ????대?吏"
+    },
+    "초피나무": {
+      src: "./assets/images/plants/초피나무.jpg",
+      alt: "초피나무 ????대?吏"
+    },
+    "측백나무": {
+      src: "./assets/images/plants/측백나무.jpg",
+      alt: "측백나무 ????대?吏"
+    },
+    "층층나무": {
+      src: "./assets/images/plants/층층나무.jpg",
+      alt: "층층나무 ????대?吏"
+    },
+    "팥배나무": {
+      src: "./assets/images/plants/팥배나무.jpg",
+      alt: "팥배나무 ????대?吏"
+    },
+    "팽나무": {
+      src: "./assets/images/plants/팽나무.jpg",
+      alt: "팽나무 ????대?吏"
+    },
+    "피나무": {
+      src: "./assets/images/plants/피나무.jpg",
+      alt: "피나무 ????대?吏"
+    },
+    "화살나무": {
+      src: "./assets/images/plants/화살나무.jpg",
+      alt: "화살나무 ????대?吏"
+    },
+    "황칠나무": {
+      src: "./assets/images/plants/황칠나무.jpg",
+      alt: "황칠나무 ????대?吏"
+    },
+    "회잎나무": {
+      src: "./assets/images/plants/회잎나무.jpg",
+      alt: "회잎나무 ????대?吏"
+    },
+    "히어리": {
+      src: "./assets/images/plants/히어리.jpg",
+      alt: "히어리 ????대?吏"
+    }
+  },
   byScientificName: {}
 };
